0e88ea14db88e3dafdf4f3e46aa72f5ddd303d0c
Martin Ždila <m.zdila@gmail.com>
2014-05-12 10:57:11 +0200

    tests

--- src/test/java/org/mule/templates/integration/BidirectionalUserSyncTestIT.java
+++ src/test/java/org/mule/templates/integration/BidirectionalUserSyncTestIT.java
@@ -1,6 +1,7 @@
 package org.mule.templates.integration;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,6 +26,7 @@ import org.mule.transport.NullPayload;
 import org.mule.util.UUID;
 
 import com.mulesoft.module.batch.BatchTestHelper;
+import com.sforce.soap.partner.UpsertResult;
 
 /**
  * The objective of this class is validating the correct behavior of the flows
@@ -34,17 +36,18 @@ import com.mulesoft.module.batch.BatchTestHelper;
 @SuppressWarnings("unchecked")
 public class BidirectionalUserSyncTestIT extends AbstractTemplatesTestCase {
 
-	private static final String ANYPOINT_TEMPLATE_NAME = "user-bidirectional-sync";
+	private static final String ANYPOINT_TEMPLATE_NAME = "usr-bidi-sync";
 	private static final String A_INBOUND_FLOW_NAME = "triggerSyncFromAFlow";
 	private static final String B_INBOUND_FLOW_NAME = "triggerSyncFromBFlow";
 	private static final int TIMEOUT_MILLIS = 60;
 
-	private SubflowInterceptingChainLifecycleWrapper updateUserInAFlow;
-	private SubflowInterceptingChainLifecycleWrapper updateUserInBFlow;
+	private SubflowInterceptingChainLifecycleWrapper upsertUserInAFlow;
+	private SubflowInterceptingChainLifecycleWrapper insertUserInBFlow;
 	private InterceptingChainLifecycleWrapper queryUserFromAFlow;
 	private InterceptingChainLifecycleWrapper queryUserFromBFlow;
 	private BatchTestHelper batchTestHelper;
 
+	private List<Map<String, Object>> createdUsersInA = new ArrayList<Map<String, Object>>();
 	private List<Map<String, Object>> createdUsersInB = new ArrayList<Map<String, Object>>();
 
 	@BeforeClass
@@ -82,12 +85,12 @@ public class BidirectionalUserSyncTestIT extends AbstractTemplatesTestCase {
 
 	private void getAndInitializeFlows() throws InitialisationException {
 		// Flow for updating a user in A instance
-		updateUserInAFlow = getSubFlow("updateUserInAFlow");
-		updateUserInAFlow.initialise();
+		upsertUserInAFlow = getSubFlow("upsertUserInAFlow");
+		upsertUserInAFlow.initialise();
 
 		// Flow for updating a user in B instance
-		updateUserInBFlow = getSubFlow("updateUserInBFlow");
-		updateUserInBFlow.initialise();
+		insertUserInBFlow = getSubFlow("insertUserInBFlow");
+		insertUserInBFlow.initialise();
 
 		// Flow for querying the user in A instance
 		queryUserFromAFlow = getSubFlow("queryUserFromAFlow");
@@ -101,23 +104,22 @@ public class BidirectionalUserSyncTestIT extends AbstractTemplatesTestCase {
 	@Test
 	public void whenUpdatingAnUserInInstanceBTheBelongingUserGetsUpdatedInInstanceA() throws MuleException, Exception {
 		Map<String, Object> user_0_B = new HashMap<String, Object>();
+		String infixB = "_0_B_" + ANYPOINT_TEMPLATE_NAME + "_" + System.currentTimeMillis();
 		user_0_B.put("Id", UUID.getUUID());
-		user_0_B.put("Username", "Username1@bar.com");
-		user_0_B.put("FirstName", "FirstName1");
-		user_0_B.put("LastName", "LastName1");
-		user_0_B.put("Email", "foo121@bar.com");
+		user_0_B.put("Username", "Name" + infixB + "@example.com");
+		user_0_B.put("FirstName", "fm" + infixB);
+		user_0_B.put("LastName", "ln" + infixB);
+		user_0_B.put("Email", "email" + infixB + "@example.com");
 		user_0_B.put("ProfileId", "00e80000001C34eAAC");
-		user_0_B.put("Alias", "alias1");
+		user_0_B.put("Alias", "alias0B");
 		user_0_B.put("TimeZoneSidKey", "GMT");
 		user_0_B.put("LocaleSidKey", "en_US");
 		user_0_B.put("EmailEncodingKey", "ISO-8859-1");
 		user_0_B.put("LanguageLocaleKey", "en_US");
+		user_0_B.put("CommunityNickname", "cn" + infixB);
 		createdUsersInB.add(user_0_B);
 		
-		SubflowInterceptingChainLifecycleWrapper createAccountInAFlow = getSubFlow("insertUserInBFlow");
-		createAccountInAFlow.initialise();
-	
-		createAccountInAFlow.process(getTestEvent(user_0_B, MessageExchangePattern.REQUEST_RESPONSE));
+		insertUserInBFlow.process(getTestEvent(user_0_B, MessageExchangePattern.REQUEST_RESPONSE));
 	
 		// Execution
 		executeWaitAndAssertBatchJob(B_INBOUND_FLOW_NAME);
@@ -127,6 +129,31 @@ public class BidirectionalUserSyncTestIT extends AbstractTemplatesTestCase {
 		Assert.assertNotNull("Synchronized user should not be null", payload);
 		Assert.assertEquals("The user should have been sync and new name must match", user_0_B.get("FirstName"), payload.get("FirstName"));
 		Assert.assertEquals("The user should have been sync and new title must match", user_0_B.get("LastName"), payload.get("LastName"));
+		
+
+		Map<String, Object> user_0_A = new HashMap<String, Object>();
+		String infixA = "_0_A_" + ANYPOINT_TEMPLATE_NAME + "_" + System.currentTimeMillis();
+		user_0_A.put("Username", "Name" + infixA + "@example.com");
+		user_0_A.put("FirstName", "fn" + infixA);
+		user_0_A.put("LastName", "ln" + infixA);
+		user_0_A.put("Email", "email" + infixA + "@example.com");
+		user_0_A.put("ProfileId", "00e80000001C34eAAC");
+		user_0_A.put("Alias", "alias0A");
+		user_0_A.put("TimeZoneSidKey", "GMT");
+		user_0_A.put("LocaleSidKey", "en_US");
+		user_0_A.put("EmailEncodingKey", "ISO-8859-1");
+		user_0_A.put("LanguageLocaleKey", "en_US");
+		user_0_A.put("CommunityNickname", "cn" + infixA);
+		createdUsersInA.add(user_0_A);
+
+		MuleEvent event = upsertUserInAFlow.process(getTestEvent(Collections.singletonList(user_0_A), MessageExchangePattern.REQUEST_RESPONSE));
+		user_0_A.put("Id", (((UpsertResult) ((List<?>) event.getMessage().getPayload()).get(0))).getId());
+
+		// Execution
+		System.out.println("foo: " + System.currentTimeMillis());
+		executeWaitAndAssertBatchJob(A_INBOUND_FLOW_NAME);
+		System.out.println("bar: " + System.currentTimeMillis());
+
 	}
 
 	private Object queryUser(Map<String, Object> user, InterceptingChainLifecycleWrapper queryUserFlow) throws MuleException, Exception {
