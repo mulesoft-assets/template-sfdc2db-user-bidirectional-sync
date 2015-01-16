3c158bc3bf8228c8dc0845d19587828ddd1601e5
Miroslav Rusin <miroslav.rusin@hotovo.org>
2015-01-16 14:52:23 +0100

    SIT-869, fix for bamboo plan

--- src/test/java/org/mule/templates/util/DateUtilsTest.java
+++ src/test/java/org/mule/templates/util/DateUtilsTest.java
@@ -20,7 +20,7 @@ public class DateUtilsTest {
 	public void dateAIsBeforeThedateBInDifferentTimezoneZulu() throws TransformerException {
 		String dateA = "2013-12-09T05:00:33.001Z";
 		Timestamp dateB = new Timestamp(new Date().getTime());
-		Assert.assertTrue("The date B should be after the date A", DateUtils.compareDates(dateA, dateB, "+01:00"));
+		Assert.assertTrue("The date B should be after the date A", DateUtils.compareDates(dateA, dateB, "01:00"));
 	}
 
 }
